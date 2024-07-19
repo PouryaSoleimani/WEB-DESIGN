0315dd586549541fc63cfa38e9756ad446fb60ff
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-19 20:36:01 +0330

    WEB DESIGN

--- app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.from('#HEADER__BACKGROUND__LAYER__1', {
-        y: "90vh",
-        duration: 2.5,
-        ease: "power2.out",
-    })
-});
